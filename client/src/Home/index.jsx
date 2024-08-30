38e264ca6c1c306414d271cda1c2bd8d0f42eea5
henr1qwe <129616999+Hner1qwe@users.noreply.github.com>
2024-08-30 18:22:45 -0300

    Update index.jsx

--- client/src/Home/index.jsx
+++ client/src/Home/index.jsx
@@ -4,16 +4,16 @@ import '../globals.css';
 export default function Home() {
     return (
         <div className='container'>
-            <h2>Sistema Acadêmico</h2>
+            <h2>Pokédex</h2>
             <div className="card-container">
                 <Link to="/matricula/cadastrar" className="card">
-                    <div>Registrar Matrícula</div>
+                    <div>Registrar Pokémon</div>
                 </Link>
                 <Link to="/matriculas" className="card">
-                    <div>Lista de Matrícula</div>
+                    <div>Pokédex</div>
                 </Link>
                 <Link to="/matriculas/alterar" className="card">
-                    <div>Editar Matrícula</div>
+                    <div>Editar Pokémon</div>
                 </Link>
             </div>
         </div>

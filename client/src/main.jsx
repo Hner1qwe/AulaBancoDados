3886d905a90f10452be62eec81e9375c50739867
henr1qwe <129616999+Hner1qwe@users.noreply.github.com>
2024-08-30 18:22:07 -0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,22 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home'
-import CreateMatricula from './CriarMatricula'
-import ReadMatriculas from './ListarMatricula'
-import UpdateMatricula from './AlterarMatricula'
+import Createpokedex from './CriarMatricula'
+import Readpokedex from './ListarMatricula'
+import Updatepokedex from './AlterarMatricula'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
             <Routes>
                   <Route path="/" element={ <Home/> }/>
-                  <Route path="/matricula/cadastrar" element={ <CreateMatricula/> }/>
-                  <Route path="/matriculas" element={ <ReadMatriculas/> }/>
-                  <Route path="/matriculas/alterar" element={ <UpdateMatricula/>}/>
+                  <Route path="/matricula/cadastrar" element={ <Createpokedex/> }/>
+                  <Route path="/matriculas" element={ <Readpokedex/> }/>
+                  <Route path="/matriculas/alterar" element={ <Updatepokedex/>}/>
             </Routes> 
       </BrowserRouter>
   </React.StrictMode>,
 )
-
-
-

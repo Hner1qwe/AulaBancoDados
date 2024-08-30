543b7f0a5cbf91fef9df1778eaf63bec44f6f2fd
henr1qwe <129616999+Hner1qwe@users.noreply.github.com>
2024-08-30 18:23:05 -0300

    Update index.jsx

--- client/src/CriarMatricula/index.jsx
+++ client/src/CriarMatricula/index.jsx
@@ -3,18 +3,20 @@ import '../globals.css';
 import { useNavigate } from 'react-router-dom';
 
 
-export default function CreateMatricula() {
-  const [aluno, setAluno] = useState('');
-  const [turma, setTurma] = useState('');
-  const [curso, setCurso] = useState('');
+export default function Createpokedex() {
+  const [imagem, setimagem] = useState('');
+  const [nome, setnome] = useState('');
+  const [numero_pokedex, setnumero] = useState('');
+  const [tipagem, settipagem] = useState('');
+  const [descricao, setdescricao] = useState('');
 
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const novaMatricula = { aluno, turma, curso };
+    const novaMatricula = { imagem, nome, numero_pokedex, tipagem, descricao };
 
     try {
-      const response = await fetch('http://localhost:5000/matriculas', {
+      const response = await fetch('http://localhost:5000/pokedex', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -23,10 +25,12 @@ export default function CreateMatricula() {
       });
       if (response.ok) {
         alert('Matrícula criada com sucesso!');
-        setAluno('');
-        setTurma('');
-        setCurso('');
-        navigate("/matriculas");
+        setimagem('');
+        setnome('');
+        setnumero('');
+        settipagem('');
+        setdescricao('');
+        navigate("/pokedex");
       } else {
         alert('Erro ao criar matrícula.');
       }
@@ -37,31 +41,45 @@ export default function CreateMatricula() {
 
   return (
     <div className='container'>
-    <form  className="form-container" onSubmit={handleSubmit}>
-      <h2>Criar Matrícula</h2>
-      <input
-        type="text"
-        placeholder="Nome do Aluno"
-        value={aluno}
-        onChange={(e) => setAluno(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Turma"
-        value={turma}
-        onChange={(e) => setTurma(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        placeholder="Curso"
-        value={curso}
-        onChange={(e) => setCurso(e.target.value)}
-        required
-      />
-      <button type="submit">Criar Matrícula</button>
-    </form>
+      <form className="form-container" onSubmit={handleSubmit}>
+        <h2>Adicionar Pokemon</h2>
+        <input
+          type="text"
+          placeholder="Imagem"
+          value={imagem}
+          onChange={(e) => setimagem(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Nome do Pokemon"
+          value={nome}
+          onChange={(e) => setnome(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Número Pokédex"
+          value={numero_pokedex}
+          onChange={(e) => setnumero(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Tipagem"
+          value={tipagem}
+          onChange={(e) => settipagem(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="Descrição"
+          value={descricao}
+          onChange={(e) => setdescricao(e.target.value)}
+          required
+        />
+        <button type="submit">Criar Matrícula</button>
+      </form>
     </div>
   );
 }
